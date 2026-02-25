cef113f7eeec5dfb36c48f420697d04094b0aaf9
musaraf0101 <nowsathmusaraf037@gmail.com>
2026-02-25 19:59:23 +0530

    token based login created

---
+++ backend/src/middleware/verifyToken.js
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+import { User } from "./../models/User.js";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.token || req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Access denied. No token provided.",
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decode.userId);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    req.userId = user._id;
+
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token expired. Please login again.",
+      });
+    }
+    return res.status(401).json({
+      success: false,
+      message: "Authentication failed",
+    });
+  }
+};
